ac9a1d8a12a68f384561f1c3acd31a6439e2dd8f
manaaal <61651618+manaaal@users.noreply.github.com>
2022-09-12 09:49:49 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,9 +1,3 @@
-"""import requests
-text = "hello good morning stupid people"
-endpoint = "https://languagetest.herokuapp.com/"
-req = requests.post(endpoint,json={'text':text})
-
-req.json()"""
 
 print("hello world !")
 
