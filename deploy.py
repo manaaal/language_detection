6d3ec8f724ecb87daa2f02483c6e71a554899e6a
manaaal <61651618+manaaal@users.noreply.github.com>
2022-09-12 10:08:20 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,8 +1,12 @@
-
-print("hello world !")
-
+import requests
 from flask import Flask, app
 app = Flask(__name__)
+
+text = "hello good morning stupid people"
+endpoint = "https://languagetest.herokuapp.com/"
+req = requests.post(endpoint,json={'text':text})
+
+#req.json()
 
 @app.route("/")
 def hello():
@@ -13,4 +17,4 @@ def test():
     return "Hello from test!"
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=82)
+    main()
